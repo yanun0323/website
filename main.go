6930a4469c8376063a6df28bd6435fa782a41f9a
yanun0323 <a1256212562@gmail.com>
2022-07-10 02:15:35 +0800

    add

--- main.go
+++ main.go
@@ -75,8 +75,8 @@ func main() {
 
 	http.HandleFunc("/", sayhelloName) //設定存取的路由
 	//設定監聽的埠
-
-	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
+	if err != nil {
 		fmt.Println(err)
 		log.Fatal("ListenAndServe: ", err)
 	}
